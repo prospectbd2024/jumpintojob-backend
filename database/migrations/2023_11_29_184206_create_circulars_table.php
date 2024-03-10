6dfd110075bb2394a00854487de14c3764b1b8a8
Ahmed Kibria <gkibria121@gmail.com>
2024-03-10 20:01:11 +0600

    bug fix, email removed for circulars

--- database/migrations/2023_11_29_184206_create_circulars_table.php
+++ database/migrations/2023_11_29_184206_create_circulars_table.php
@@ -17,7 +17,6 @@ return new class extends Migration {
             $table->string('title');
             $table->string('description');
             $table->string('availability');
-            $table->string('email');
             $table->string('phone');
             $table->string('slug')->unique();
             $table->string('current_company_name');
