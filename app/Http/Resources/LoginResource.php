2bc268959bc19418a3bd10681ba91054f524950f
Ahmed Kibria <gkibria121@gmail.com>
2024-03-10 19:54:21 +0600

    is account verified

--- app/Http/Resources/LoginResource.php
+++ app/Http/Resources/LoginResource.php
@@ -13,6 +13,7 @@ class LoginResource extends JsonResource
             'result' => true,
             'message' => ($this->resource->email_verified_at == null) ? ', Please verify your email address to use all features!' : 'Successfully logged in',
             'token_type' => 'Bearer',
+            'is_verified' => $this->resource->is_verified,
             'expires_at' => now()->addMinutes(config('sanctum.expiration'))->toDateTimeString(), // Add this line,
             'access_token' => $this->resource->createToken('API Token')->plainTextToken,
             'user_type' => $this->resource->user_type,
