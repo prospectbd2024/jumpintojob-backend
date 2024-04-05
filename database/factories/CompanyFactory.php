fdfa66460454a1d3337dcb93683382dee6eae87c
Ahmed Kibria <gkibria121@gmail.com>
2024-04-05 10:11:04 +0600

    bug fix

--- database/factories/CompanyFactory.php
+++ database/factories/CompanyFactory.php
@@ -13,7 +13,7 @@ class CompanyFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => $this->faker->name($maxNbChars = 50), 
+            'name' => $this->faker->name($maxNbChars = 20), 
             'logo' => $this->faker->imageUrl(),
             'email' => $this->faker->unique()->safeEmail(),
             'phone' => $this->faker->unique()->phoneNumber(),
