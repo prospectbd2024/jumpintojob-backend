96fd9284fc24f0da265be0ed01bf0111428cd3c2
Ahmed Kibria <gkibria121@gmail.com>
2024-05-24 18:36:02 +0600

    classic template added

--- resources/views/templates/classic.blade.php
+++ resources/views/templates/classic.blade.php
@@ -38,7 +38,7 @@
         .contact-info {
             display: flex;
             justify-content: space-between;
-            width: 600px;
+            max-width: 800px;
         }
 
         .contact-item {
@@ -77,7 +77,10 @@
             position: relative;
             padding-left: 20px;
         }
-
+        .experties-list{
+            list-style: circle;
+        }
+     
         .experience-details li::before,
         .education-details li::before {
             content: "•";
@@ -88,7 +91,9 @@
             transform: translateX(-100%);
 
         }
-
+        .experties-list li::before{
+            content: none;
+        }
         hr {
             height: 3px;
             background: black;
@@ -128,7 +133,7 @@
         }
         .skills-container {
     width: 70%; /* Adjust width as needed */
-    text-align: center;
+
     }
 
     .skills-container h2 {
@@ -169,6 +174,9 @@
         align-items: center;
     }
 
+        .hide {
+            display: none;
+        }
     </style>
     <title>classic</title>
 </head>
@@ -176,17 +184,21 @@
 <body>
 
     <header>
-        <h2 class="header-title">AIDEN KELLY</h2>
-        <div class="header-subtitle">High School Teacher | Sciences</div>
+        <h2 class="header-title">{{ $resume_data['personalInformation']['firstName'] }} {{ $resume_data['personalInformation']['lastName'] }}</h2>
+        <div class="header-subtitle">{{ $resume_data['personalInformation']['title'] }}</div>
         <div class="contact-info">
             <div>
-                <i class="fa fa-phone contact-item"></i> +3598888888888
+                <i class="fa fa-phone contact-item"></i>{{$resume_data['personalInformation']['phone']}}
             </div>
             <div>
-                <span class="contact-item">@</span> name@gmail.com
+                <span class="contact-item">@</span>{{$resume_data['personalInformation']['email']}}
             </div>
             <div>
-                <i class="fa fa-map-marker contact-item"></i> Queens,NY
+                <i class="fa fa-map-marker contact-item"></i>  
+                {{ $resume_data['personalInformation']['currentAddress']['postalCode'] }},
+                {{ $resume_data['personalInformation']['currentAddress']['city'] }},
+                {{ $resume_data['personalInformation']['currentAddress']['state'] }},
+                {{ $resume_data['personalInformation']['currentAddress']['country'] }}
             </div>
         </div>
     </header>
@@ -195,79 +207,63 @@
             <div class="experience-section">
                 <h2>EXPERIENCE</h2>
                 <hr>
-                <div>
-                    <h3>Teacher</h3>
-                    <p class="highlight">Hudson High School of Learning Technologies</p>
+                @foreach ($resume_data['experiences']  as $experience)
+                <div class="{{$experience['visible_on_cv']? '' : 'hide' }}">
+                    <h3>{{ $experience['job_title'] }}</h3>
+                    <p class="highlight">{{ $experience['company_name']}}</p>
                     <div class="experience-period">
-                        <span><i class="fa fa-calendar"></i> 01/2015- Present</span>
-                        <span style="margin-left: 10px;"><i class="fa fa-map-marker"></i> NYC</span>
+                        <span><i class="fa fa-calendar"></i> 
+                        @php
+                            echo $experience['start_date'] ;
+                            echo  $experience['currently_working'] ? '- Present' : $experience['to_date']; 
+                        @endphp
+                        
+                        </span>
+                        <span style="margin-left: 10px;"><i class="fa fa-map-marker"></i> {{$experience['company_location']}}</span>
                     </div>
                     <ul class="experience-details">
-                        <li>Teaching classes of 25+ on biology and chemistry topics</li>
-                        <li>Participated in student recruitment, registration and placement activities</li>
-                        <li>Coordinated School Information Night each year</li>
-                        <li>Contributed to raising retention rate from 75% - 89% through running extracurricular
-                            sessions</li>
-                        <li>Received two outstanding reports from classroom inspections from the city central education
-                            board</li>
-                        <li>Ran 100+ school information sessions</li>
+                        <li>{{$experience['responsibilities']}}</li>
+                        <li>
+                            <p>Experties</p>
+                            <ul class="experties-list">
+                                @foreach ($experience['expertises'] as $expertise)
+                                     
+                                <li>
+                                    {{$expertise['name']}} for {{$expertise['months']}} months
+                                </li>   
+                                @endforeach
+                            </ul>
+                            </li>
                     </ul>
-                    <hr class="subsection-end-hr">
-                </div>
-
-                <div>
-                    <h3>Biology Teacher</h3>
-                    <p class="highlight">Boston High School</p>
-                    <div class="experience-period">
-                        <span><i class="fa fa-calendar"></i> 01/2013 - 12/2015</span>
-                        <span style="margin-left: 10px;"><i class="fa fa-map-marker"></i> Boston,MA</span>
-                    </div>
-                    <ul class="experience-details">
-                        <li>Developed and executed daily lesson plans to engage and challenge student understanding and
-                            involvement, including 30+ international students (ESL) and students with specialized
-                            educational needs.</li>
-                        <li>Increased the number of A+ to C grades from 60% to 90% over 2 years</li>
-                        <li>Taught and mentored 100+ students over the two years, and led 5 extra learning classes
-                            outside of school hours</li>
-                        <li>Engaged in peer collaboration and instruction during staff development opportunities as well
-                            as peer observation of classroom strategies and assessment.</li>
-                        <li>Designed original student learning plans centered on the curriculum with corresponding
-                            lectures and lab activities which aligned with the Next Generation Science Standards.</li>
-                    </ul>
-                    <hr class="subsection-end-hr">
-                </div>
-
-                <div>
-                    <h3>Teaching Assistant</h3>
-                    <p class="highlight">Boston High School</p>
-                    <div class="experience-period">
-                        <span><i class="fa fa-calendar"></i> 2009-2013</span>
-                        <span style="margin-left: 10px;">Boston, MA</span>
-                    </div>
-                    <ul class="experience-details">
-                        <li>Implemented lesson plans independently for classrooms of 25+ students</li>
-                        <li>Provide educational materials, including daily lesson plans and weekly homework packets that
-                            averaged 95% completion rate</li>
-                        <li>Lectured weekly in tutorials, and regularly in courses over 8 semesters</li>
-                        <li>Assist professor and a class of 25 students with the course related needs</li>
-                    </ul>
-                </div>
+                    <hr class="subsection-end-hr {{ $loop->last ? 'hide' : '' }}">
+                </div>             
+                @endforeach
             </div>
 
             <div class="education-section">
                 <h2>EDUCATION</h2>
                 <hr>
-                <div>
-                    <div class="education-container">
+
+                @foreach  ($resume_data['educations']  as $education)
+                <div class=" {{$education['visible_on_cv']? '' : 'hide' }}"> 
+                    <div class="education-container ">
                         <div >
-                            <h3>Doctorate of Education in Educational Administration</h3>
-                            <p class="highlight">South Carolina State University</p>
+                            <h2>{{$education['field_study']}}</h2>
+                            <h3>{{$education['degree']}}</h3>
+                            <p class="highlight">{{$education['institution_name']}}</p>
                             <div class="experience-period">
-                                <span><i class="fa fa-calendar"></i> 2011-2013</span>
-                                <span style="margin-left: 10px;">Orangeburg, SC</span>
+                                <span><i class="fa fa-calendar"></i>
+                                
+                                @php
+                                    echo   $education['education_starting_year'];
+                                    echo   $education['education_graduation_year']!=""?$education['education_graduation_year']:"- Present";
+                                @endphp
+                                
+                                </span>
+                                <span style="margin-left: 10px;">{{ $education['institution_location']}} </span>
                             </div>
                             <ul class="education-details">
-                                <li>Excellence Award(2013)</li>
+                                <li>{{ $education['education_achievements']}}</li>
                             </ul>
                         </div>
                         <div class="education-gpa-container">
@@ -281,16 +277,10 @@
                         </div>
                     </div>
 
-                    <hr class="subsection-end-hr">
+                    <hr class="subsection-end-hr {{ $loop->last ? 'hide' : '' }}">
                 </div>
+                @endforeach
 
-                <div>
-                    <h3>Certificate of Education(Career Development)</h3>
-                    <p class="highlight">South Carolina State University</p>
-                    <div class="experience-period">
-                        <span><i class="fa fa-calendar"></i> 2010-2011</span>
-                    </div>
-                </div>
             </div>
         </div>
 
@@ -298,47 +288,37 @@
             <div class="certificate-section">
                 <h2>CERTIFICATION</h2>
                 <hr>
+                
+                @foreach ($resume_data['others'] as $other)
+                @if ($other['type'] =='Certificate')                    
                 <div>
-                    <h3 class="header-subtitle">Professional Registration</h3>
-                    <p>Career Development Association of UNited States</p>
+                    <h3 class="header-subtitle">{{$other['title']}}</h3>
+                    <p>{{$other['description']}}</p>
                     <hr class="subsection-end-hr">
                 </div>
-                <div>
-                    <h3 class="header-subtitle">Proficient Teacher</h3>
-                    <p>NY Education Standards Authority</p>
-                    <hr class="subsection-end-hr">
-                </div>
-                <div>
-                    <h3 class="header-subtitle">White Card & Asbestos Awareness</h3>
-                    <p>MIT</p>
-                    <br>
-                </div>
-                <div>
-                    <h3 class="header-subtitle">Senior First Aid</h3>
-                    <p>Ablaze Training Solutions</p>
-                    <hr class="subsection-end-hr">
-                </div>
-                <div>
-                    <h3  class="header-subtitle">Anaphylaxis Training</h3>
-                    <p>ACSIA</p>
-                </div>
+                @endif
+                    
+                @endforeach
             </div>
 
 
             <div>
                 <h2>PUBLICATIONS</h2>
                 <hr>
+                
+                @foreach ($resume_data['others'] as $other)
+                @if ($other['type'] =='Publication')      
                 <div>
-                    <h3 class="publication-subtitle">Project: Issues/Benefits in a Blended Biology Course</h3>
-                    <p class="header-subtitle">Global Science and Technology Forum</p>
-                    <p><span> <i class="fa fa-calendar"></i> 09/2014 </span></p>
-                    <hr>
+                    <h3 class="publication-subtitle">Project: {{$other['title']}}</h3>
+                    <p class="header-subtitle">{{$other['journal']}}</p>
+                    <p><span> <i class="fa fa-calendar"></i> {{$other['date']}}</span></p>
+                    <p>{{$other['abstract']}}</p>
+                    <hr class="subsection-end-hr">
                 </div>
-                <div>
-                    <h3 class="publication-subtitle">Biology Models & Mechanisms</h3>
-                    <p class="header-subtitle">Burgmann Journal(ANU Press)</p>
-                    <p><span> <i class="fa fa-calendar"></i> 09/2012 </span></p>
-                </div>
+
+                @endif
+                @endforeach
+
 
             </div>
 
@@ -346,24 +326,45 @@
                 <h2>SKILLS</h2>
                 <hr>
                 <ul>
-                    <li>PowerPoint</li>
-                    <li>Photoshop</li>
-                    <li>Cloud Storage</li>
-                    <li>Trello</li>
-                    <li>Microsoft Teams</li>
-                    <li>Blackboard</li>
+                     @foreach ($resume_data['skills'] as $skill)
+                         <li>{{$skill['name']}}</li>
+                     @endforeach
+                </ul>
+
+            </div>
+            <div class="skills-container">
+                <h2>LANGUAGES</h2>
+                <hr>
+                <ul>
+                     @foreach ($resume_data['languages'] as $language)
+                         <li>{{$language['language']}}</li>
+                     @endforeach
+                </ul>
+
+            </div>
+            <div class="skills-container">
+                <h2>HOBBIES</h2>
+                <hr>
+                <ul>
+                     @foreach ($resume_data['hobbies'] as $hobby)
+                         <li>{{$hobby['name']}}</li>
+                     @endforeach
                 </ul>
 
             </div>
 
 
-
             <div>
-                <h2>VOLUNTEERING</h2>
-                <h3 class="publication-subtitle">Volunteer</h3>
-                <p class="header-subtitle">Peace Corps</p>
-                <p><span> <i class="fa fa-calendar"></i> 2007 -2008 </span></p>
-                <p style="margin-top: 2px;">Organized Kitchen Welcome for people in need in Queens, NYC</p>
+                <h2>OTHERS</h2>
+                @foreach ($resume_data['others'] as $other)
+                @if ($other['type'] =='Other')      
+                <div>
+                    <h3 class="publication-subtitle"> {{$other['title']}}</h3>
+                    <p style="margin-top: 2px;"> {{$other['description']}}</p>
+                    <hr class="subsection-end-hr">
+                </div>
+                @endif
+                @endforeach
             </div>
 
 
