51a322540df53a925bb1b5483e17975121aaff85
gkibria121 <gkibria121@gmail.com>
2024-07-14 09:06:16 +0600

    add more templates

--- resources/views/templates/modern.blade.php
+++ resources/views/templates/modern.blade.php
@@ -3,245 +3,181 @@
 
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Resume</title>
     <style>
-        * {
-            margin: 0;
-            padding: 0;
-            box-sizing: border-box;
-        }
-
         body {
-            font-family: Arial, Helvetica, sans-serif;
-            padding: 40px 100px;
-            background-color: #f7f7f7;
+            font-family: Arial, sans-serif;
+            background-color: #f4f4f9;
+            margin: 0;
+            padding: 10px;
+            color: #524e4e;
         }
 
-        header {
-            margin-bottom: 40px;
-            text-align: center;
+        .container {
+            max-width: 800px;
+            height: 970px;
+            margin: 20px auto;
+            background-color: #ffffff;
+            padding: 60px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
         }
 
-        .header-title {
-            font-size: 32px;
-            margin-bottom: 5px;
-        }
-
-        .header-subtitle {
-            color: #d49548;
-            font-size: 18px;
-            font-weight: bold;
-            margin-bottom: 10px;
-        }
-
-        .contact-info {
+        .header {
             display: flex;
-            justify-content: center;
-            gap: 30px;
-            color: #555;
+            justify-content: space-between;
+            align-items: flex-start;
+            margin-bottom: 20px;
         }
 
-        .contact-info div {
-            display: flex;
-            align-items: center;
-            gap: 5px;
+        .header h1 {
+            margin: 0;
+            font-size: 2.5em;
         }
 
-        .contact-info i {
-            color: #d49548;
+        .header .contact-info {
+            text-align: left;
+            width: 35%;
+            font-size: large;
+            font-weight: 550;
         }
 
-        main {
+        .section {
+            margin-bottom: 0px;
+        }
+
+        .section h2 {
+            font-size: 1.6em;
+            color: skyblue;
+        }
+
+        .column-container {
             display: flex;
             justify-content: space-between;
         }
 
-        .main-section {
-            width: 48%;
+        .left-column {
+            width: 60%;
         }
 
-        h2 {
-            color: #2c3e50;
-            border-bottom: 2px solid #d49548;
-            padding-bottom: 5px;
-            margin-bottom: 20px;
+        .right-column {
+            width: 35%;
         }
 
-        h3 {
-            color: #2c3e50;
-            margin-bottom: 5px;
-        }
-
-        .highlight {
-            color: #d49548;
-            font-weight: bold;
-        }
-
-        .experience-period,
-        .education-period {
-            display: flex;
-            align-items: center;
-            gap: 10px;
+        .right-column .section {
             margin-bottom: 10px;
-            color: #555;
         }
 
-        .experience-details,
-        .education-details {
-            list-style: none;
-            margin-bottom: 20px;
-            padding-left: 20px;
-            color: #333;
-        }
-
-        .experience-details li::before,
-        .education-details li::before {
-            content: "•";
-            color: #d49548;
-            position: absolute;
-            left: -15px;
-        }
-
-        .subsection-end-hr {
-            border: none;
-            border-top: 1px dashed grey;
-            margin-bottom: 20px;
-            height: 0px;
-            background-color: transparent;
-            margin-top: 10px;
-        }
-
-        .skills-container ul,
-        .languages-container ul,
-        .hobbies-container ul,
-        .certificates-container ul {
+        .section ul {
             list-style: none;
             padding: 0;
-            display: flex;
-            flex-wrap: wrap;
-            gap: 10px;
         }
 
-        .skills-container li,
-        .languages-container li,
-        .hobbies-container li,
-        .certificates-container li {
-            background-color: #fff;
-            padding: 10px;
-            border: 1px solid #ddd;
-            border-radius: 5px;
-            font-weight: bold;
+        .section ul li {
+            margin-bottom: 15px;
+        }
+
+        .clear {
+            clear: both;
         }
     </style>
-    <title>Resume</title>
 </head>
 
 <body>
-    <header>
-        <h2 class="header-title">{{ $resume->personal_informations['firstName'] }} {{ $resume->personal_informations['lastName'] }}</h2>
-        <div class="header-subtitle">{{ $resume->personal_informations['title'] }}</div>
-        <div class="contact-info">
-            <div><i class="fa fa-phone"></i>{{$resume->personal_informations['phone']}}</div>
-            <div><span>@</span>{{$resume->personal_informations['email']}}</div>
-            <div><i class="fa fa-map-marker"></i> {{ $resume->personal_informations['currentAddress']['postalCode'] }}, {{ $resume->personal_informations['currentAddress']['city'] }}, {{ $resume->personal_informations['currentAddress']['state'] }}, {{ $resume->personal_informations['currentAddress']['country'] }}</div>
+    <div class="container">
+        <div class="header">
+            <div class="name-title">
+                <h1>{{ $resume->personal_informations['firstName'] }} {{ $resume->personal_informations['lastName'] }}</h1>
+                <p style="font-size: large; font-weight: 550;">{{ $resume->personal_informations['title'] }}</p>
+            </div>
+            <div class="contact-info">
+                <p>
+                    {{ $resume->personal_informations['currentAddress']['postalCode'] }}, {{ $resume->personal_informations['currentAddress']['city'] }}, {{ $resume->personal_informations['currentAddress']['state'] }}, {{ $resume->personal_informations['currentAddress']['country'] }}<br>
+                    {{ $resume->personal_informations['email'] }}<br>
+                    {{ $resume->personal_informations['phone'] }}<br>
+                </p>
+            </div>
         </div>
-    </header>
-    <main>
-        <div class="main-section">
-            <div class="experience-section {{ count($resume->experiences)==0 ? 'hide' : ''}}">
-                <h2>Experience</h2>
-                @foreach ($resume->experiences as $experience)
-                <div class="{{ $experience['visible_on_cv'] ? '' : 'hide' }}">
-                    <h3>{{ $experience['job_title'] }}</h3>
-                    <p class="highlight">{{ $experience['company_name'] }}</p>
-                    <div class="experience-period">
-                        <span><i class="fa fa-calendar"></i> 
-                        @php
-                            echo $experience['start_date'] ;
-                            echo $experience['currently_working'] ? '- Present' : $experience['to_date']; 
-                        @endphp
-                        </span>
-                        <span><i class="fa fa-map-marker"></i> {{$experience['company_location']}}</span>
-                    </div>
-                    <ul class="experience-details">
-                        <li>{{$experience['responsibilities']}}</li>
-                        <li>
-                            <p>Expertise</p>
-                            <ul class="experties-list">
-                                @foreach ($experience['expertises'] as $expertise)
-                                <li>{{ $expertise['name'] }} for {{ $expertise['months'] }} months</li>
-                                @endforeach
-                            </ul>
+        <div class="column-container">
+            <div class="left-column">
+                <div class="summary section">
+                    <h2>Summary</h2>
+                    <p>Lorem ipsum In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available</p>
+                </div>
+                <div class="education section {{ count($resume->educations)==0 ? 'hide' : ''}}">
+                    <h2>Education</h2>
+                    <ul>
+                        @foreach ($resume->educations as $education)
+                        <li class="{{ $education['visible_on_cv'] ? '' : 'hide' }}">
+                            <strong>{{ $education['institution_name'] }}, {{ $education['institution_location'] }} — {{ $education['degree'] }} in {{ $education['field_study'] }}</strong><br>
+                            @php
+                                echo $education['education_starting_year'];
+                                echo $education['education_graduation_year'] ? '-' . $education['education_graduation_year'] : '- Present';
+                            @endphp
+                            <br>{{ $education['education_achievements'] }}
                         </li>
+                        @endforeach
                     </ul>
-                    <hr class="subsection-end-hr {{ $loop->last ? 'hide' : '' }}">
                 </div>
-                @endforeach
+                <div class="projects section {{ count($resume->projects)==0 ? 'hide' : ''}}">
+                    <h2>Projects</h2>
+                    <ul>
+                        @foreach ($resume->projects as $project)
+                        <li class="{{ $project['visible_on_cv'] ? '' : 'hide' }}">
+                            <strong>{{ $project['title'] }} — Detail</strong><br>
+                            {{ $project['description'] }}
+                        </li>
+                        @endforeach
+                    </ul>
+                </div>
             </div>
-
-            <div class="education-section {{ count($resume->educations)==0 ? 'hide' : ''}}">
-                <h2>Education</h2>
-                @foreach ($resume->educations as $education)
-                <div class="{{ $education['visible_on_cv'] ? '' : 'hide' }}">
-                    <h3>{{ $education['degree'] }} in {{ $education['field_study'] }}</h3>
-                    <p class="highlight">{{ $education['institution_name'] }}</p>
-                    <div class="education-period">
-                        <span><i class="fa fa-calendar"></i>
-                        @php
-                            echo $education['education_starting_year'];
-                            echo $education['education_graduation_year'] ? '-' . $education['education_graduation_year'] : '- Present';
-                        @endphp
-                        </span>
-                        <span>{{ $education['institution_location'] }}</span>
-                    </div>
-                    <ul class="education-details">
-                        <li>{{ $education['education_achievements'] }}</li>
+            <div class="right-column">
+                <div class="skills section {{ count($resume->skills)==0 ? 'hide' : ''}}">
+                    <h2>Skills</h2>
+                    <ul style="list-style: circle; margin-left: 15px;">
+                        @foreach ($resume->skills as $skill)
+                        <li>{{ $skill['name'] }}</li>
+                        @endforeach
                     </ul>
-                    <hr class="subsection-end-hr {{ $loop->last ? 'hide' : '' }}">
                 </div>
-                @endforeach
+                <div class="awards section {{ count($resume->awards)==0 ? 'hide' : ''}}">
+                    <h2>Awards</h2>
+                    <ul>
+                        @foreach ($resume->awards as $award)
+                        <li>
+                            {{ $award['title'] }}<br>
+                            {{ $award['description'] }}
+                        </li>
+                        @endforeach
+                    </ul>
+                </div>
+                <div class="languages section {{ count($resume->languages)==0 ? 'hide' : ''}}">
+                    <h2>Languages</h2>
+                    <ul style="list-style: circle; margin-left: 15px;">
+                        @foreach ($resume->languages as $language)
+                        <li>{{ $language['language'] }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+                <div class="experience section {{ count($resume->experiences)==0 ? 'hide' : ''}}">
+                    <h2>Experience</h2>
+                    <ul>
+                        @foreach ($resume->experiences as $experience)
+                        <li class="{{ $experience['visible_on_cv'] ? '' : 'hide' }}">
+                            <strong>{{ $experience['job_title'] }} - {{ $experience['company_name'] }}</strong><br>
+                            @php
+                                echo $experience['start_date'];
+                                echo $experience['currently_working'] ? '- Present' : $experience['to_date'];
+                            @endphp
+                            <br>{{ $experience['responsibilities'] }}
+                        </li>
+                        @endforeach
+                    </ul>
+                </div>
             </div>
         </div>
-
-        <div class="main-section">
-            <div class="skills-container {{ count($resume->skills)==0 ? 'hide' : ''}}">
-                <h2>Skills</h2>
-                <ul>
-                    @foreach ($resume->skills as $skill)
-                    <li>{{ $skill['name'] }}</li>
-                    @endforeach
-                </ul>
-            </div>
-
-            <div class="languages-container {{ count($resume->languages)==0 ? 'hide' : ''}}">
-                <h2>Languages</h2>
-                <ul>
-                    @foreach ($resume->languages as $language)
-                    <li>{{ $language['language'] }}</li>
-                    @endforeach
-                </ul>
-            </div>
-
-            <div class="hobbies-container {{ count($resume->hobbies)==0 ? 'hide' : ''}}">
-                <h2>Hobbies</h2>
-                <ul>
-                    @foreach ($resume->hobbies as $hobby)
-                    <li>{{ $hobby['name'] }}</li>
-                    @endforeach
-                </ul>
-            </div>
-
-            <div class="certificates-container {{ count($resume->certificates)==0 ? 'hide' : ''}}">
-                <h2>Certificates</h2>
-                <ul>
-                    @foreach ($resume->certificates as $certificate)
-                    <li>{{ $certificate['title'] }}</li>
-                    @endforeach
-                </ul>
-            </div>
-        </div>
-    </main>
+        <div class="clear"></div>
+    </div>
 </body>
 
 </html>
