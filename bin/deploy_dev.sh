94d59fe205ca80be8d938e1d1969ccf1d5315429
gkibria121 <gkibria121@gmail.com>
2025-04-16 00:17:36 +0600

    wip

--- bin/deploy_dev.sh
+++ bin/deploy_dev.sh
@@ -4,7 +4,7 @@ if [[ ! -e .env ]]
 then
     cp .env.example-dev .env
 fi
-docker compose $files down
+docker compose $files down -v
 docker compose $files up -d --build
 docker compose exec backend sh ./bin/update_dev.sh
 echo 'http://localhost:8090/'
