fbd28b1c13c38a769c72d34056317e5918458337
gkibria121 <gkibria121@gmail.com>
2025-08-27 11:29:12 +0600

    add

--- bin/update_dev.sh
+++ bin/update_dev.sh
@@ -1,9 +1,4 @@
-#!/bin/bash
-composer install
+#!/bin/bash 
 php artisan migrate --seed --force
 php artisan optimize:clear
-php artisan optimize
-ln -s /app/storage/app/public  /app/public/storage 
-chown -R www-data:www-data /app/storage /app/bootstrap/cache  
-chmod -R 775 /app/storage /app/bootstrap/cache
-chmod -R 777 /app/storage/logs
+php artisan optimize 
